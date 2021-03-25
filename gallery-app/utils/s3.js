534aed95304caba903cd403219e8ddf5041ee1ab
vinita2000 <vinita.yadav@gladiris.com>
2021-03-25 10:29:28 +0530

    added some files

--- gallery-app/utils/s3.js
+++ gallery-app/utils/s3.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
-const bucketName = "gallery-vinita";
-const region = "ap-south-1";
-const accessKeyId = "AKIA5HUZFVGBFCA2FRW3";
-const secretAccessKey = "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";
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.ACCESS_KEY_ID;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 
 const s3 = new S3({
     region,
